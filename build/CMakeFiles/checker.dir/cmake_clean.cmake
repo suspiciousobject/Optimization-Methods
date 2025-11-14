661e6f40d9e7f355e05d3ba3fa50faca46637096
su5p0bj3ct <85576353+suspiciousobject@users.noreply.github.com>
2025-11-14 11:26:44 +0300

    Add files via upload
    Adding new CMakeLists.txt and build folder

---
+++ build/CMakeFiles/checker.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/checker.dir/link.d"
+  "CMakeFiles/checker.dir/src/checker.cpp.o"
+  "CMakeFiles/checker.dir/src/checker.cpp.o.d"
+  "bin/checker"
+  "bin/checker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/checker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
